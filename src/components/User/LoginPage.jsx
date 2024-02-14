24a113c97d4d4265f06162503b865df3b23771c2
03rohit98 <rohit.singh@gamersback.com>
2024-02-14 17:20:50 +0530

    build LoginPage

---
+++ src/components/User/LoginPage.jsx
@@ -0,0 +1,16 @@
+import Header from "../Header/Header";
+
+const Loginpage = () => {
+  return (
+    <div>
+      <Header />
+      <img
+        className="bg-cover w-[2000px]"
+        src="https://t4.ftcdn.net/jpg/02/32/16/07/360_F_232160763_FuTBWDd981tvYEJFXpFZtolm8l4ct0Nz.jpg"
+        alt="background-image"
+      />
+    </div>
+  );
+};
+
+export default Loginpage;
